91d75ce78b7101a0e55cd15a72d55da6af2c7ff7
Dylan <drn7344@nyu.edu>
2024-05-09 11:23:39 +0800

    Last

---
+++ project-b-template/js/album.js
@@ -0,0 +1,40 @@
+let images = [];
+let currentIndex = 0;
+
+function preload() {
+    
+    images.push(loadImage('image1.png'));
+    images.push(loadImage('image2.png'));
+    images.push(loadImage('image3.png'));
+    images.push(loadImage('image4.png'));
+   
+    images.push(loadImage('image5.png'));
+}
+
+function setup() {
+  let cnv = createCanvas(600, 400);
+    cnv.parent("canvasContainer");
+}
+
+function draw() {
+    background(220);
+
+  
+    image(images[currentIndex], 0, 0, width, height);
+
+
+    fill(0);
+    text(`Image ${currentIndex + 1} / ${images.length}`, 10, 20);
+}
+
+function keyPressed() {
+    
+    if (keyCode === LEFT_ARROW) {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+    }
+
+
+    if (keyCode === RIGHT_ARROW) {
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+}
